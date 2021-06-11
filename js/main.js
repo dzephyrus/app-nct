053c320262b961e3e177f170bf4fa3260b5f8d25
dzephyrus <57651733+dzephyrus@users.noreply.github.com>
2021-06-11 15:20:33 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var countGridItem = document.querySelectorAll('.grid-item').length;
-		if(countGridItem>4){
-			$( ".grid2" ).removeClass( "change-grid" );
-		   }
-		else{
-			$( ".grid2" ).addClass( "change-grid" );
-			$( ".grid-desc" ).removeClass( "col-8 col-sm-9 col-12 col-sm-10 " );
-		}
+    if($('.grid2').hasClass('home-grid')){
+    }
+    else{
+        if(countGridItem>4){
+            $( ".grid2" ).removeClass( "change-grid" );
+           }
+        else{
+            $( ".grid2" ).addClass( "change-grid" );
+            $( ".grid-desc" ).removeClass( "col-8 col-sm-9 col-12 col-sm-10 " );
+        }
+	}
+
 //		console.log(countGridItem);
 
 $(window).bind("resize", function () {
